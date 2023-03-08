b2ea2c05599312d0dbdfa5d3290357f08a160ce2
NguyenDuy2205 <119591781+NguyenDuy2205@users.noreply.github.com>
2023-03-08 17:22:45 +0700

    Add files via upload
    create card

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Chị';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 8/3';
+const titleCard = 'Món quà tặng bạn ngày 8/3';
+const contentCard = 'Chúc mừng ngày 8/3, Chúc chị mạnh khoẻ, hạnh phúc, ngày càng xinh đẹp, gặt hái được nhiều thành công trong công việc và hạnh phúc bên gia đình';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = '4.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
